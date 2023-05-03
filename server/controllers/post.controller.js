d7b5a96d3bc33894c6d7f49d23bc288fc252d533
Ashok Kumar <ap271449@gmail.com>
2023-05-03 18:51:22 +0530

    connected post with user + delte route

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,4 +1,5 @@
 const postModel = require("../models/post.model");
+const UserModel = require("../models/user.model")
 
 module.exports = {
 
@@ -7,7 +8,15 @@ module.exports = {
         const payload = req.body;
         try {
             let post = new postModel(payload);
+
+            // get the user to update post in userModal
+            let user = await UserModel.findById(post.user);
+            user.posts.push(post._id);
+
+            // save user and post to DB
+            await user.save();
             await post.save();
+
             res.status(200).send({ message: "post added successfully" });
         } catch (e) {
             console.log(e);
@@ -32,7 +41,7 @@ module.exports = {
         let payload = req.body;
         try {
             let post = await postModel.findByIdAndUpdate(id, payload);
-            res.json({ message: "post is updated successfully" });
+            res.json({ message: "post is updated successfully", post });
         } catch (error) {
             console.log(error);
             res.status(500).send({ message: error.message });
@@ -43,8 +52,24 @@ module.exports = {
     deletePost: async (req, res) => {
         let id = req.params.id;
         try {
-            let post = await postModel.findByIdAndDelete(id);
+            // get the "post" for retrieve the user id
+            let post = await postModel.findById(id);
+
+            // find user using user id from "post"
+            let user = await UserModel.findById(post.user);
+
+            // remove post id from user's "post" array
+            let index = user.posts.indexOf(post._id);
+            if (index > -1) user.posts.splice(index, 1);
+            else return res.status(404).send({ message: "post not found for this user" });
+
+            // remove "post" from "posts" collection
+            await postModel.findByIdAndDelete(id);
+
+            // save updated user DB
+            await user.save();
             res.json({ message: "post is deleted successfully" });
+
         } catch (error) {
             console.log(error);
             res.status(500).send({ message: error.message });
